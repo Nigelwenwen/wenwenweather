3b20583516a7c70ae5fc9568441bcfe4fa2b83e1
Nigel <838319690@qq.com>
2017-12-10 10:03:49 +0800

    加入数据库和表的各项配置。

---
+++ app/src/main/java/com/example/zzw/wenwenweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.zzw.wenwenweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zzw on 2017/12/10.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
