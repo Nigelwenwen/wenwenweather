31045d7780725dba13c43afa71168d9880a809b6
Nigel <838319690@qq.com>
2017-12-11 19:15:47 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/zzw/wenwenweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.zzw.wenwenweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zzw on 2017/12/10.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
